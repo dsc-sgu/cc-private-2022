670db49bcca7cbec6e6113997fc922e388cb120f
ForestGump <74667646+EremeevTim2004@users.noreply.github.com>
2022-12-06 21:24:39 +0400

    Добавил переменные для дальнейшей попытки фикса

--- Projects/ants-colony/main.cpp
+++ Projects/ants-colony/main.cpp
@@ -1,6 +1,7 @@
 #include <raylib-ext.hpp>
 #include <vector>
 #include <iostream>
+#include <algorithm>
 #include <cassert>
 #include <cmath>
 
@@ -10,16 +11,25 @@
 #define ANT_ATTRACTION_BASE (0)
 #define EPS (1e-5)
 
-const int wheight = 600;
-const int wwidth = 800;
-const int cell_r = 2;
+const int WindowsHeight = 600;
+const int WindowsWidth = 800;
 
+const int cell_radius = 2;
+const int cell_fresh_period; // можно double для более точной настройки
 
 struct cell_t
 {
-    int x, y;
+    int x;
+    int y;
+    
     uint8_t home_marker;
     uint8_t food_marker;
+    
+    // NODE(ForestGump): с помощью этого возможно удасться решить проблему
+    unsigned fresh; // Свежесть
+    unsigned order; // Порядок
+    double distance_to_home_market; // Дистонция до дома/муровейника
+    double distance_to_food_market; // Дистанция до метки с едой
 };
 
 enum ant_state_t
@@ -31,98 +41,122 @@ enum ant_state_t
 struct ant_t
 {
     Vector2 position;
-    ant_state_t state;
     Vector2 direction;
+
+    ant_state_t state;
+
     float speed;
     float dir_cooldown;
     float marker_cooldown;
     float attraction_cooldown;
+
+    // NODE(ForestGump):
+    // Количество меток, оставленных муравьём 
+    // Если муравей пришёл без еды, то метки удаляем, а счётчик обнуляем
+    int cell_count;
 };
 
 struct food_source_t
 {
     int charges;
+    
     float radius;
+
     Vector2 position;
 };
 
 struct colony_t
 {
     float radius;
+    
     Vector2 position;
+
     uint32_t size;
 };
 
 typedef std::vector<std::vector<cell_t>> field_t;
 
-void
-print_cell(cell_t &cell)
+void print_cell(cell_t &cell)
 {
-    std::cout << "Home: " << (int) cell.home_marker
-              << "; Food: " << (int) cell.food_marker << std::endl;
+    std::cout << "Home: " << (int) cell.home_marker << "; Food: " << (int) cell.food_marker << std::endl << "; Freshness: " << (int)cell.fresh << "; Order: " << (int)cell.order;
 }
 
-float
-calculate_attractiveness(Vector2 point, ant_state_t state, field_t &field)
+float calculate_attractiveness(Vector2 point, ant_state_t state, field_t &field)
 {
     Vector2 cells;
-    cells.x = wwidth / cell_r;
-    cells.y = wheight / cell_r;
+    
+    cells.x = WindowsWidth / cell_radius;
+    cells.y = WindowsHeight / cell_radius;
 
     int row = int(point.x);
     int col = int(point.y);
 
     int square_half = 1;
+
     std::vector<cell_t *> neighbors;
+
     for (int i = -square_half; i <= square_half; ++i)
     {
         for (int j = -square_half; j <= square_half; ++j)
         {
             int x = i + row;
             int y = j + col;
-            if (x < 0 || x >= (int) cells.x || y < 0 || y >= (int) cells.y)
-                continue;
+            
+            if (x < 0 || x >= (int) cells.x || y < 0 || y >= (int) cells.y) continue;
+
             neighbors.push_back(&field[x][y]);
         }
     }
 
     std::pair<float, float> attractiveness;
+
     for (cell_t *neighbor : neighbors)
     {
         attractiveness.first += (float) neighbor->home_marker;
         attractiveness.second += (float) neighbor->food_marker;
     }
-    // std::cout << attractiveness.first / float(neighbors.size()) << std::endl;
-    // std::cout << neighbors.size() << std::endl;
+    
     if (neighbors.size() == 0) return 0;
+    
     switch (state)
     {
-    case GOING_HOME:
-        return attractiveness.first / float(neighbors.size());
-    case SEARCHING_FOOD:
-        return attractiveness.second / float(neighbors.size());
-    default:
-        assert(false);
+        case GOING_HOME:
+        {
+            return attractiveness.first / float(neighbors.size());
+        }
+        case SEARCHING_FOOD:
+        {
+            return attractiveness.second / float(neighbors.size());
+        }
+        default:
+        {
+            // NODE(ForestGump):
+            // Тут не нужен break или что-то типа того?
+            // Или assert может выполнять туже функцию?
+            assert(false);
+        }
     }
 }
 
 std::vector<Vector2> attractive_points(3);
 
-cell_t *
-find_attractive_cell2(ant_t &ant, field_t &field)
+cell_t *find_attractive_cell2(ant_t &ant, field_t &field)
 {
     Vector2 cells;
-    cells.x = wwidth / cell_r;
-    cells.y = wheight / cell_r;
+    
+    cells.x = WindowsWidth / cell_radius;
+    cells.y = WindowsHeight / cell_radius;
 
     Vector2 ant_screen = ant.position;
-    int row = (int) (ant_screen.x / cell_r);
-    int col = (int) (ant_screen.y / cell_r);
+    
+    int row = (int) (ant_screen.x / cell_radius);
+    int col = (int) (ant_screen.y / cell_radius);
 
     row = std::clamp(row, 0, int(cells.x) - 1);
     col = std::clamp(col, 0, int(cells.y) - 1);
 
-    float scale = cell_r * 20;
+    float scale = cell_radius * 20;
+
     Vector2 center = ant.position + ant.direction * scale;
     Vector2 left = ant.position + Vector2Rotate(ant.direction,  45 * DEG2RAD) * scale;
     Vector2 right = ant.position + Vector2Rotate(ant.direction, -45 * DEG2RAD) * scale;
@@ -133,100 +167,114 @@ find_attractive_cell2(ant_t &ant, field_t &field)
 
     if (std::abs(center_attr) < EPS && std::abs(left_attr) < EPS && std::abs(right_attr) < EPS)
     {
-        // std::cout << "not found" << std::endl;
         return nullptr;
     }
     else if (center_attr >= std::max(left_attr, right_attr))
     {
-        // std::cout << "center: " << center_attr << ' ' << left_attr << ' ' << right_attr << std::endl;
-        return &field[int(center.x) / cell_r][int(center.y) / cell_r];
+        return &field[int(center.x) / cell_radius][int(center.y) / cell_radius];
     }
     else if (left_attr > right_attr)
     {
-        // std::cout << "left: " << center_attr << ' ' << left_attr << ' ' << right_attr << std::endl;
-        return &field[int(left.x) / cell_r][int(left.y) / cell_r];
+        return &field[int(left.x) / cell_radius][int(left.y) / cell_radius];
     }
     else
     {
-        // std::cout << "right: " << center_attr << ' ' << left_attr << ' ' << right_attr << std::endl;
-        return &field[int(right.x) / cell_r][int(right.y) / cell_r];
+        return &field[int(right.x) / cell_radius][int(right.y) / cell_radius];
     }
 }
 
-cell_t *
-find_attractive_cell(ant_t &ant, field_t &field)
+cell_t *find_attractive_cell(ant_t &ant, field_t &field)
 {
     Vector2 cells;
-    cells.x = wwidth / cell_r;
-    cells.y = wheight / cell_r;
+
+    cells.x = WindowsWidth / cell_radius;
+    cells.y = WindowsHeight / cell_radius;
 
     Vector2 ant_screen = ant.position;
-    int row = (int) (ant_screen.x / cell_r);
-    int col = (int) (ant_screen.y / cell_r);
+
+    int row = (int) (ant_screen.x / cell_radius);
+    int col = (int) (ant_screen.y / cell_radius);
 
     row = std::clamp(row, 0, int(cells.x) - 1);
     col = std::clamp(col, 0, int(cells.y) - 1);
 
     int square_half = 5;
+    
     std::vector<cell_t *> neighbors;
+
     for (int i = -square_half; i <= square_half; ++i)
     {
         for (int j = -square_half; j <= square_half; ++j)
         {
             int x = i + row;
             int y = j + col;
-            if (x < 0 || x >= (int) cells.x || y < 0 || y >= (int) cells.y)
-                continue;
+            
+            if (x < 0 || x >= (int) cells.x || y < 0 || y >= (int) cells.y) continue;
+            
             neighbors.push_back(&field[x][y]);
         }
     }
 
+    // NODE(ForestGump): Что это, почему 1е9?
     float min_val = 1e9;
+    
     cell_t *cell = nullptr;
+
     for (cell_t *neighbor : neighbors)
     {
         float a = std::abs(row - neighbor->x);
         float b = std::abs(col - neighbor->y);
+
         float distance = sqrt(a * a + b * b);
         float value;
+
         switch (ant.state)
         {
-        case GOING_HOME:
-            value = neighbor->home_marker / distance;
-            if (neighbor->home_marker > 0 && value < min_val)
+            case GOING_HOME:
             {
-                cell = neighbor;
-                min_val = value;
+                value = neighbor->home_marker / distance;
+
+                if (neighbor->home_marker > 0 && value < min_val)
+                {
+                    cell = neighbor;
+                    min_val = value;
+                }
+
+                break;
             }
-            break;
-        case SEARCHING_FOOD:
-            value = neighbor->food_marker / distance;
-            if (neighbor->food_marker > 0 && value < min_val)
+            case SEARCHING_FOOD:
             {
-                cell = neighbor;
-                min_val = value;
+                value = neighbor->food_marker / distance;
+
+                if (neighbor->food_marker > 0 && value < min_val)
+                {
+                    cell = neighbor;
+                    min_val = value;
+                }
+
+                break;
             }
-            break;
         }
     }
+
     return cell;
 }
 
-void
-ant_update(ant_t &ant, field_t &field, std::vector<food_source_t> &sources,
-           colony_t &colony, float dt)
+void ant_update(ant_t &ant, field_t &field, std::vector<food_source_t> &sources, colony_t &colony, float dt)
 {
     ant.dir_cooldown -= dt;
     ant.marker_cooldown -= dt;
     ant.attraction_cooldown -= dt;
 
     Vector2 cells;
-    cells.x = wwidth / cell_r;
-    cells.y = wheight / cell_r;
+    
+    cells.x = WindowsWidth / cell_radius;
+    cells.y = WindowsHeight / cell_radius;
 
     Vector2 ant_screen = ant.position;
-    int row = (int) (ant_screen.x / cell_r);
-    int col = (int) (ant_screen.y / cell_r);
+
+    int row = (int) (ant_screen.x / cell_radius);
+    int col = (int) (ant_screen.y / cell_radius);
 
     row = std::clamp(row, 0, int(cells.x) - 1);
     col = std::clamp(col, 0, int(cells.y) - 1);
@@ -234,6 +282,7 @@ ant_update(ant_t &ant, field_t &field, std::vector<food_source_t> &sources,
     if (ant.dir_cooldown <= 0)
     {
         float rotation = (std::rand() % 20 - 10) * DEG2RAD;
+
         ant.dir_cooldown = ANT_WANDER_BASE;
         ant.direction = Vector2Rotate(ant.direction, rotation);
         ant.direction = Vector2Normalize(ant.direction);
@@ -242,77 +291,73 @@ ant_update(ant_t &ant, field_t &field, std::vector<food_source_t> &sources,
     if (ant.marker_cooldown <= 0)
     {
         cell_t &cell = field[row][col];
+        
         ant.marker_cooldown = ANT_MARKER_BASE;
+
         switch (ant.state)
         {
-        case GOING_HOME:
-            cell.food_marker = 255;
-            break;
-        case SEARCHING_FOOD:
-            cell.home_marker = 255;
-            break;
+            case GOING_HOME:
+            {
+                cell.food_marker = 255;
+
+                break;
+            }
+            case SEARCHING_FOOD:
+            {
+                cell.home_marker = 255;
+                
+                break;
+            }
         }
     }
 
     if (ant.attraction_cooldown <= 0)
     {
         cell_t *attractor = find_attractive_cell2(ant, field);
+
         if (attractor != nullptr)
         {
             Vector2 attractor_screen_pos;
-            attractor_screen_pos.x = attractor->x * cell_r + cell_r / 2.0f;
-            attractor_screen_pos.y = attractor->y * cell_r + cell_r / 2.0f;
-            // std::cout << attractor_screen_pos.x << ' ' << attractor_screen_pos.y << std::endl;
+           
+            attractor_screen_pos.x = attractor->x * cell_radius + cell_radius / 2.0f;
+            attractor_screen_pos.y = attractor->y * cell_radius + cell_radius / 2.0f;
+
             Vector2 to_attractor = attractor_screen_pos - ant.position;
-            if (Vector2LengthSqr(to_attractor) > 0.00001)
-                to_attractor = Vector2Normalize(to_attractor);
+            
+            if (Vector2LengthSqr(to_attractor) > 0.00001) to_attractor = Vector2Normalize(to_attractor);
+            
             ant.direction = to_attractor;
-            // {
-            //     Vector2 p1 = local_to_screen(ant.position);
-            //     Vector2 p2 = local_to_screen(ant.position + ant.direction * 20);
-            //     DrawCircleV(attractor_screen_pos, 2, GREEN);
-            //     DrawLineEx(p1, p2, 2, GREEN);
-            // }
         }
+
         ant.attraction_cooldown = ANT_ATTRACTION_BASE;
     }
 
     ant.position += ant.direction * (ant.speed * dt);
 
-    if (ant.position.x >= wwidth || ant.position.x <= 0)
-    {
-        ant.direction.x *= -1;
-    }
-
-    if (ant.position.y >= wheight || ant.position.y <= 0)
-    {
-        ant.direction.y *= -1;
-    }
+    if (ant.position.x >= WindowsWidth || ant.position.x <= 0) ant.direction.x *= -1;
+    if (ant.position.y >= WindowsHeight || ant.position.y <= 0) ant.direction.y *= -1;
 
     for (auto &source : sources)
     {
         if (CheckCollisionPointCircle(ant.position, source.position, source.radius))
         {
             ant.state = GOING_HOME;
+
             break;
         }
     }
 
-    if (CheckCollisionPointCircle(ant.position, colony.position, colony.radius))
-    {
-        ant.state = SEARCHING_FOOD;
-    }
+    if (CheckCollisionPointCircle(ant.position, colony.position, colony.radius)) ant.state = SEARCHING_FOOD;
 }
 
-int
-main()
+int main()
 {
-    InitWindow(wwidth, wheight, "Ants Colony");
+    InitWindow(WindowsWidth, WindowsHeight, "Ants Colony");
     SetTargetFPS(60);
 
     Vector2 cells;
-    cells.x = wwidth / cell_r;
-    cells.y = wheight / cell_r;
+    cells.x = WindowsWidth / cell_radius;
+    cells.y = WindowsHeight / cell_radius;
 
     field_t field((int) cells.x, std::vector<cell_t>((int) cells.y));
 
@@ -322,22 +367,27 @@ main()
     colony.size = 1000;
 
     std::vector<ant_t> ants;
+
     for (int i = 0; i < colony.size; ++i)
     {
         ant_t ant;
         ant.position = colony.position;
         ant.state = SEARCHING_FOOD;
         ant.direction = {1, 0};
+
         float rotation = rand() % 360;
+
         ant.direction = Vector2Rotate(ant.direction, rotation * DEG2RAD);
         ant.speed = 60;
         ant.dir_cooldown = ANT_WANDER_BASE;
         ant.marker_cooldown = ANT_MARKER_BASE;
         ant.attraction_cooldown = ANT_ATTRACTION_BASE;
+
         ants.push_back(ant);
     }
 
     std::vector<food_source_t> sources;
+
     for (int i = 0; i < 1; ++i)
     {
         food_source_t source;
@@ -360,28 +410,32 @@ main()
     }
 
     float fadeout_timeout = MARKER_FADEOUT_BASE;
+
     while (!WindowShouldClose())
     {
         float dt = GetFrameTime();
+    
         BeginDrawing();
         {
             ClearBackground(BLACK);
+
             for (int x = 0; x < (int) cells.x; ++x)
             {
                 for (int y = 0; y < (int) cells.y; ++y)
                 {
                     cell_t cell = field[x][y];
+                    
                     auto r = cell.home_marker;
                     auto g = cell.food_marker;
+                    
                     Color color = { r, g, 0, 255 };
-                    DrawRectangle(x * cell_r, y * cell_r, cell_r, cell_r, color);
+
+                    DrawRectangle(x * cell_radius, y * cell_radius, cell_radius, cell_radius, color);
                 }
             }
 
-            {
-                DrawCircleV(colony.position, colony.radius, RED);
-            }
-
+            DrawCircleV(colony.position, colony.radius, RED);
+            
             for (auto &source : sources)
             {
                 DrawCircleV(source.position, source.radius, GREEN);
@@ -391,15 +445,11 @@ main()
             {
                 ant_update(ant, field, sources, colony, dt);
                 DrawCircleV(ant.position, 2, WHITE);
-                // {
-                //     Vector2 p1 = local_to_screen(ant.position);
-                //     Vector2 p2 = local_to_screen(ant.position + ant.direction * 20);
-                //     // DrawCircleV(attractor_screen_pos, 2, GREEN);
-                //     DrawLineEx(p1, p2, 2, GREEN);
-                // }
+
                 if (ant.state == GOING_HOME && false)
                 {
-                    float scale = cell_r * 20;
+                    float scale = cell_radius * 20;
+
                     Vector2 center = ant.position + ant.direction * scale;
                     Vector2 left = ant.position + Vector2Rotate(ant.direction,  45 * DEG2RAD) * scale;
                     Vector2 right = ant.position + Vector2Rotate(ant.direction, -45 * DEG2RAD) * scale;
@@ -414,6 +464,7 @@ main()
                 }
             }
         }
+
         EndDrawing();
 
         fadeout_timeout -= dt;
@@ -425,16 +476,17 @@ main()
                 for (int x = 0; x < (int) cells.x; ++x)
                 {
                     cell_t &cell = field[x][y];
-                    if (cell.home_marker > 0)
-                        cell.home_marker -= 1;
-                    if (cell.food_marker > 0)
-                        cell.food_marker -= 1;
+
+                    if (cell.home_marker > 0) cell.home_marker -= 1;
+                    if (cell.food_marker > 0) cell.food_marker -= 1;
                 }
             }
+
             fadeout_timeout = MARKER_FADEOUT_BASE;
         }
     }
 
     CloseWindow();
+    
     return 0;
 }
